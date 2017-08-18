51a8cc8362cdb92723b4d1a51be8bbf7a5aea59b
Cesar Gomes Reche <cesar.reche@techne.com.br>
2017-08-18 15:01:07 -0300

    terrain

--- Cinetico/src/render/resourcedata.cpp
+++ Cinetico/src/render/resourcedata.cpp
@@ -2,28 +2,48 @@
 #include "renderengine.h"
 #include "resourcedata.h"
 
+#include <cstring>
+
 ResourceData::ResourceData(RenderEngine *engine, int resId)
 : RenderObject(engine,resId)
 {
+	m_vertices = NULL;
+	m_indices = NULL;
+	m_colors = NULL;
 	m_vertexCount = 0;
 	m_indexCount = 0;
 	m_colors = NULL;
 }
 
 void ResourceData::setVertices(unsigned int vertexCount, Vertex3 vertices[]) {
+	if(m_vertices)
+		delete [] m_vertices;
+	
+	m_vertices = new Vertex3[vertexCount];
+	::memcpy(m_vertices,vertices,vertexCount*sizeof(Vertex3));
 	m_vertexCount = vertexCount;
-	m_vertices = vertices;
+	
 	m_dirtyFlags |= VERTEX_DIRTY;
 }
 
 void ResourceData::setIndices(unsigned int indexCount, int indices[]) {
+	if(m_indices)
+		delete [] m_indices;
+	
+	m_indices = new int[indexCount];
+	::memcpy(m_indices,indices,indexCount*sizeof(int));
 	m_indexCount = indexCount;
-	m_indices = indices;
+
 	m_dirtyFlags |= INDEX_DIRTY;
 }
 
 void ResourceData::setColors(Color *colors) {
-	m_colors = colors;
+	if(m_colors)
+		delete [] m_colors;
+	
+	m_colors = new Color[m_vertexCount];
+	::memcpy(m_colors,colors,m_vertexCount*sizeof(Color));
+	
 	m_dirtyFlags |= COLOR_DIRTY;
 }
 
